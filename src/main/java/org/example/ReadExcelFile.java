fe4cc08c668a0effb3f0d0377665b7ca6b1d2849
rk800 <becreatative@gmail.com>
2024-04-02 17:54:01 +0500

    Лабораторная работа №10. Программирование

---
+++ src/main/java/org/example/ReadExcelFile.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+
+public class ReadExcelFile {
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/main/java/org/example/example-excel.xlsx";
+        FileInputStream inputStream = new FileInputStream(filePath);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+
+        XSSFSheet sheet = workbook.getSheet("Товары");
+
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                System.out.print(cell.toString() + "\t");
+            }
+            System.out.println();
+        }
+
+        workbook.close();
+        inputStream.close();
+    }
+}
